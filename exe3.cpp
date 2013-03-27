58bf95f522f930e66ea7d8c73eef40ae62453a4a
leivid <leividm@gmail.com>
2013-03-27 18:04:06 -0300

    Update exe3.cpp

--- exe3.cpp
+++ exe3.cpp
@@ -7,7 +7,7 @@
 
 
 typedef struct Aluno{
-  int cod_aluno;
+	int cod_aluno;
 	char nome_aluno[40];
 }TAluno;
 
@@ -49,17 +49,71 @@ void ExibeListasNotas(TPilha *pilha,TFila *fila_notas){
 		printf("\n\nAluno: %s cod: %d",pilha->alunos[i].nome_aluno,pilha->alunos[i].cod_aluno);
 		for(int j = 0; j < fila_notas->tamanho_fila;j++ ){
 			if(pilha->alunos[i].cod_aluno == fila_notas->notas[j].fk_cod_aluno){
-				printf("\nNota: %.2f ",fila_notas->notas[j].nota);
+				printf("\nNota: %.1f ",fila_notas->notas[j].nota);
 			}
 		}
 	}
 	getch();
 }
 
+void AlunosSemNotas(TPilha *pilha,TFila *fila_notas){
+	int temnota;
+	for(int i=0; i < pilha->topo +1;i++){
+		temnota = -1;
+		for(int j = 0; j < fila_notas->tamanho_fila;j++ ){
+			if(pilha->alunos[i].cod_aluno == fila_notas->notas[j].fk_cod_aluno){
+				temnota++;
+			}
+		}
+
+		if(temnota == -1){
+			printf("\n\nAluno: %s cod: %d",pilha->alunos[i].nome_aluno,pilha->alunos[i].cod_aluno);
+		}
+	}
+	getch();
+}
+
+void ExcluiAluno(TPilha *pilha,TFila *fila_notas){
+	int temnota = -1;
+	for(int j = 0; j < fila_notas->tamanho_fila; j++ ){
+		if(pilha->alunos[pilha->topo].cod_aluno == fila_notas->notas[j].fk_cod_aluno){
+			temnota++;
+			break;
+		}
+	}
+
+	if(temnota == -1){
+		pilha->topo--;
+		printf("\n Um aluno excluido com sucesso!\n");
+	}else{
+		printf("\n Este aluno nao pode ser excluido, pois possui notas !\n");	
+	}
+	getch();
+}
+
+void ExcluiNotaAluno(TPilha *pilha,TFila *fila_notas){
+	int temnota = -1;
+	for(int j = 0; j < fila_notas->tamanho_fila; j++ ){
+		if(pilha->alunos[pilha->topo].cod_aluno == fila_notas->notas[j].fk_cod_aluno){
+			temnota++;
+			break;
+		}
+	}
+
+	if(temnota == -1){
+		pilha->topo--;
+		printf("\n Um aluno excluido com sucesso!\n");
+	}else{
+		printf("\n Este aluno nao pode ser excluido, pois possui notas !\n");	
+	}
+	getch();
+}
+
+
 void CadastraAluno(TPilha *pilha,int *sequencia){
 	int continua;
 	do{
-		*sequencia = *sequencia +1;
+		(*sequencia)++;
 		pilha->topo++;
 		pilha->alunos[pilha->topo].cod_aluno = *sequencia;
 		printf("\nDigite o nome aluno: \n");
@@ -70,6 +124,8 @@ void CadastraAluno(TPilha *pilha,int *sequencia){
 		scanf("%d",&continua);
 	}while(continua != 2);
 }
+
+
 
 void InsereNotas(TPilha *pilha_alunos,TFila *fila_notas){
 	int aux_cod, achou,continua;
@@ -89,6 +145,9 @@ void InsereNotas(TPilha *pilha_alunos,TFila *fila_notas){
 				fila_notas->notas[fila_notas->tamanho_fila].fk_cod_aluno = aux_cod;
 				fflush(stdin);
 				scanf("%f",&fila_notas->notas[fila_notas->tamanho_fila].nota);
+				printf("\nNota cadastrada \n");
+				getch();
+				LimpaTela();
 				fila_notas->tamanho_fila++;
 				printf("Deseja adicionar mais notas p/este aluno: 1 - Sim  2 - Nao");
 				scanf("%d",&continua);
@@ -96,13 +155,45 @@ void InsereNotas(TPilha *pilha_alunos,TFila *fila_notas){
 		}
 		else
 			printf("Aluno nao encontrado.");
-
+		getch();
+		LimpaTela();
 		printf("Deseja adicionar mais notas p/ outro aluno: 1-Sim  2-Nao");
 		scanf("%d",&continua);
 	}while(continua != 2);
 
 }
 
+void CalculaMedia(TPilha *pilha_alunos,TFila *fila_notas){
+	int achou,aux_cod,continua;
+	float media,soma = 0,contador = 0;
+	
+	do{
+		LimpaTela();
+		achou = -1;
+		
+		printf("\nDigite o codigo do aluno a consulta: \n");
+		scanf("%d",&aux_cod);
+		
+		achou = Pesquisa_na_pilha(pilha_alunos,aux_cod);
+		if(achou != -1){
+			printf("\n\nAluno: %s cod: %d",pilha_alunos->alunos[achou].nome_aluno,pilha_alunos->alunos[achou].cod_aluno);
+			for(int i = 0; i < fila_notas->tamanho_fila;i++ ){
+				if(pilha_alunos->alunos[achou].cod_aluno == fila_notas->notas[i].fk_cod_aluno){
+					contador++;
+					soma = soma +fila_notas->notas[i].nota; 
+				}
+			}
+			media = soma / contador;
+			printf("\n****** Media:  %.1f  ********",media);
+		}else{
+			printf("\nAluno nao encontrado. \n");
+		}
+		getch();
+		LimpaTela();
+		printf("\nDeseja consultar outro aluno: 1-Sim 2-Nao\n");
+		scanf("%d",&continua);
+	}while(continua != 2);
+}
 
 
 void menu(){
@@ -116,7 +207,7 @@ void menu(){
 	printf("\n6-	Alterar nota");
 	printf("\n7-	Exiba lista:");
 	printf("\n8-	Exiba lista com notas:");
-	printf("\n0-	Sair");//voltar para 7 antes de entregar
+	printf("\n0-	Sair\n");//voltar para 7 antes de entregar
 }
 
 int main()
@@ -134,6 +225,9 @@ int main()
 		switch(opcao){
 			case 1: CadastraAluno(&pilha_alunos,&sequencia);break;
 			case 2: InsereNotas(&pilha_alunos,&fila_notas);break;
+			case 3: CalculaMedia(&pilha_alunos,&fila_notas);break;
+			case 4: AlunosSemNotas(&pilha_alunos,&fila_notas);break;
+			case 5: ExcluiAluno(&pilha_alunos,&fila_notas);break;
 			case 7: ExibeListas(&pilha_alunos);break;
 			case 8: ExibeListasNotas(&pilha_alunos,&fila_notas);break;
 			case 0:break;
